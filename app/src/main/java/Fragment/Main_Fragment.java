28ab49572d9859264a2148650710a9026892fca9
권준희 <apple951234@gmail.com>
2021-03-17 20:04:24 +0900

    그래프는 아직 데이터 없음

---
+++ app/src/main/java/Fragment/Main_Fragment.java
@@ -0,0 +1,18 @@
+package Fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.dagym_login.R;
+
+public class Main_Fragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.main_fragment, container, false);
+        return view;
+    }
+}
